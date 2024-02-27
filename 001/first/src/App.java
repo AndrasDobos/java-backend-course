e5d05584c665eb48afc305c460fbebf590227f91
AndrewDobos <andras.dobos@gmail.com>
2024-02-27 20:29:32 +0100

    new file:   todoapp/todoapp/.gitignore 	new file:   todoapp/todoapp/.mvn/wrapper/maven-wrapper.jar 	new file:   todoapp/todoapp/.mvn/wrapper/maven-wrapper.properties 	new file:   todoapp/todoapp/mvnw 	new file:   todoapp/todoapp/mvnw.cmd 	new file:   todoapp/todoapp/pom.xml 	new file:   todoapp/todoapp/src/main/java/com/todoapp/TodoappApplication.java 	new file:   todoapp/todoapp/src/main/resources/application.properties 	new file:   todoapp/todoapp/src/test/java/com/todoapp/TodoappApplicationTests.java

---
+++ 001/first/src/App.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int index = 0; index < array.length; index++) {
+            
+        }
+
+        for (iterable_type iterable_element : iterable) {
+            
+        }
+    }
+}
